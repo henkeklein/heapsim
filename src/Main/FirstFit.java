9db9270e96ac54c7354f0e157e239ad8fb621559
Henrik Klein <henrik.klein12@gmail.com>
2016-03-07 14:26:19 +0100

    start

---
+++ src/Main/FirstFit.java
@@ -0,0 +1,62 @@
+package Main;
+
+/**
+ * This memory model allocates memory cells based on the first-fit method.
+ *
+ * @author "Johan Holmberg, Malmö university"
+ * @since 1.0
+ */
+public class FirstFit extends Memory {
+
+    /**
+     * Initializes an instance of a first fit-based memory.
+     *
+     * @param size The number of cells.
+     */
+    public FirstFit(int size) {
+        super(size);
+        // TODO Implement this!
+    }
+
+    /**
+     * Allocates a number of memory cells.
+     *
+     * @param size the number of cells to allocate.
+     * @return The address of the first cell.
+     */
+    @Override
+    public Pointer alloc(int size) {
+        // TODO Implement this!
+        return null;
+    }
+
+    /**
+     * Releases a number of data cells
+     *
+     * @param p The pointer to release.
+     */
+    @Override
+    public void release(Pointer p) {
+        // TODO Implement this!
+    }
+
+    /**
+     * Prints a simple model of the memory. Example:
+     *
+     * |    0 -  110 | Allocated
+     * |  111 -  150 | Free
+     * |  151 -  999 | Allocated
+     * | 1000 - 1024 | Free
+     */
+    @Override
+    public void printLayout() {
+        // TODO Implement this!
+    }
+
+    /**
+     * Compacts the memory space.
+     */
+    public void compact() {
+        // TODO Implement this!
+    }
+}
